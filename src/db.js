60096dbeff738ff5c5da50291f68768c4094fb9b
Mr-Hache <hernancastillo@unicauca.edu.co>
2023-05-20 10:24:58 -0500

    deploy

--- src/db.js
+++ src/db.js
@@ -8,11 +8,21 @@ const fs = require("fs");
 const path = require("path");
 
 // The necessary variables are destructured from the environment data
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_DEPLOY } = process.env;
 
 // An instance of the Sequelize class is created using environment variables
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+//   {
+//     // set to console.log to see the raw SQL queries
+//     logging: false,
+//     // lets Sequelize know we can use pg-native for ~30% more speed
+//     native: false,
+//   }
+// );
+
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  DB_DEPLOY,
   {
     // set to console.log to see the raw SQL queries
     logging: false,

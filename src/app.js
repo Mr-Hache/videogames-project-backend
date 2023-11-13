7bc104fae7c570725a5fb42b622129a4c2b53f8f
Mr-Hache <hernancastillo@unicauca.edu.co>
2023-11-13 18:28:10 -0500

    agregando la respuesta a favicon

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const server = express();
 
 server.name = 'API';
 
+server.get('/favicon.ico', (req, res) => res.status(204));
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
